45bacafbb1dccd108ea8deb53d07be1984ada095
groupseven7 <groupzero0seven7@gmail.com>
2019-12-17 11:20:17 +0400

    added code

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -11,6 +11,12 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //added some code for push
+
+            Console.WriteLine("Enter number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+1+" I Won");
         }
     }
 }
